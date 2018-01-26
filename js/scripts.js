9ed87d83753df01d06dd9f86172f8cbbcae60295
Bjoern Schilberg <bjoern@intevation.de>
2018-01-26 12:09:47 +0100

    Migrated from csv to xlsx.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,60 @@
-Papa.parse('data/file.csv', {
-  download: true,
-  header: true,
-  complete: function(results) {
-    console.log(results);
-    var documents = results.data;
-    var idx = lunr(function() {
-      this.ref('id')
-      this.field('PLZ')
+/* set up an async GET request */
+var req = new XMLHttpRequest();
+req.open('GET', 'data/Abfallberatungen_Dropbox.xlsx', true);
+req.responseType = 'arraybuffer';
 
-      documents.forEach(function(doc) {
-        this.add(doc)
-      }, this)
-    });
+req.onload = function(e) {
+  /* parse the data when it is received */
+  var wb = XLSX.read(req.response, { type: 'array' });
 
-    new Vue({
-      el: '#app',
-      data: {
-        query: '',
-        results: [],
-        count: ''
+  var documents = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], { range: 3 });
 
-      },
-      methods: {
-        search: function() {
-          var results = [];
-          var query = this.query.toLowerCase();
-          var searchResults = [];
-          if (query) {
-            searchResults = idx.query(function(q) {
-              q.term(query, {
-                wildcard: lunr.Query.wildcard.TRAILING
-              })
-            });
-            if (searchResults.length > 0) {
-              this.count = searchResults.length;
-              for (var item in searchResults) {
-                results.push(documents.find(function(obj) { return obj.id === searchResults[item].ref }));
-              }
-              this.results = results;
-            } else {
-              this.results = [];
-              this.count = '';
+  lunr.tokenizer.separator = /[\s\-\,]+/
+
+  var idx = lunr(function() {
+    this.ref('id')
+    this.field('PLZ')
+
+    documents.forEach(function(doc) {
+      this.add(doc)
+    }, this)
+  });
+
+  new Vue({
+    el: '#app',
+    data: {
+      query: '',
+      results: [],
+      count: ''
+
+    },
+    methods: {
+      search: function() {
+        var results = [];
+        var query = this.query.toLowerCase();
+        var searchResults = [];
+        if (query) {
+          searchResults = idx.query(function(q) {
+            q.term(query, {
+              wildcard: lunr.Query.wildcard.TRAILING
+            })
+          });
+          if (searchResults.length > 0) {
+            this.count = searchResults.length;
+            for (var item in searchResults) {
+              results.push(documents.find(function(obj) { return obj.id === searchResults[item].ref }));
             }
+            this.results = results;
           } else {
             this.results = [];
             this.count = '';
           }
+        } else {
+          this.results = [];
+          this.count = '';
         }
       }
-    });
-  }
-});
+    }
+  });
+};
+req.send();
